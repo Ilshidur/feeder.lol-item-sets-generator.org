6307048d2de114899eadc3cef39f65c221b1f154
Nicolas Coutin <Ilshidur@users.noreply.github.com>
2017-08-08 10:55:29 +0200

    refactor: statsd scope

--- src/statsd.js
+++ src/statsd.js
@@ -1,37 +1,39 @@
 import Lynx from 'lynx';
 import config from './config';
 
-const metrics = new Lynx(config.statsd.host, config.statsd.port);
+const metrics = new Lynx(config.statsd.host, config.statsd.port, {
+  scope: 'lol-item-sets-genrator',
+});
 
 let generationDurationTimer = null;
 
 function registerGeneration() {
   if (config.statsd.enabled) {
-    metrics.increment('lol-item-sets-generator.builds_generation.count');
+    metrics.increment('builds_generation.count');
   }
 }
 
 function setChampionsCount(championsCount) {
   if (config.statsd.enabled) {
-    metrics.set('lol-item-sets-generator.generation.champions', championsCount);
+    metrics.set('generation.champions', championsCount);
   }
 }
 
 function setItemsCount(itemsCount) {
   if (config.statsd.enabled) {
-    metrics.set('lol-item-sets-generator.generation.items', itemsCount);
+    metrics.set('generation.items', itemsCount);
   }
 }
 
 function setBuildsCount(buildsCount) {
   if (config.statsd.enabled) {
-    metrics.set('lol-item-sets-generator.generation.items', buildsCount);
+    metrics.set('generation.items', buildsCount);
   }
 }
 
 function startGenerationTimer() {
   if (config.statsd.enabled) {
-    generationDurationTimer = metrics.createTimer('lol-item-sets-generator.generation.duration');
+    generationDurationTimer = metrics.createTimer('generation.duration');
   }
 }
 
