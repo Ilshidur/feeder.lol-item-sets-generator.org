0c0ef2846a833d54258e8d883550dcd03d896abd
Nicolas Coutin <Ilshidur@users.noreply.github.com>
2017-08-08 11:19:09 +0200

    fix: statsd metrics

--- src/statsd.js
+++ src/statsd.js
@@ -9,31 +9,31 @@ let generationDurationTimer = null;
 
 function registerGeneration() {
   if (config.statsd.enabled) {
-    metrics.increment('builds_generation.count');
+    metrics.increment('generations_count');
   }
 }
 
 function setChampionsCount(championsCount) {
   if (config.statsd.enabled) {
-    metrics.set('generation.champions', championsCount);
+    metrics.count('generation_champions', championsCount);
   }
 }
 
 function setItemsCount(itemsCount) {
   if (config.statsd.enabled) {
-    metrics.set('generation.items', itemsCount);
+    metrics.count('generation_items', itemsCount);
   }
 }
 
 function setBuildsCount(buildsCount) {
   if (config.statsd.enabled) {
-    metrics.set('generation.items', buildsCount);
+    metrics.count('generation_items', buildsCount);
   }
 }
 
 function startGenerationTimer() {
   if (config.statsd.enabled) {
-    generationDurationTimer = metrics.createTimer('generation.duration');
+    generationDurationTimer = metrics.createTimer('generation_duration');
   }
 }
 
@@ -43,6 +43,9 @@ function stopGenerationTimer() {
     generationDurationTimer = null;
   }
 }
+
+// TODO: Call 'close' :
+// https://github.com/dscape/lynx/blob/master/lib/lynx.js#L482
 
 export {
   registerGeneration,
