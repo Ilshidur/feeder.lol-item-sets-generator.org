07759f575ccb113fb120d0fb7fbe4d00e3a10e08
Ilshidur <maar-gan@hotmail.fr>
2017-08-13 01:50:04 +0200

    feat: node-statsd (test)

--- src/statsd.js
+++ src/statsd.js
@@ -1,60 +1,63 @@
-import Lynx from 'lynx';
+import StatsD from 'node-statsd';
 import config from './config';
 
-const metrics = new Lynx(config.statsd.host, config.statsd.port, {
-  scope: config.statsd.scope,
-  on_error: (err) => {
-    console.error(err.message);
-  },
+const client = new StatsD({
+  host: config.statsd.host,
+  port: config.statsd.port,
+  prefix: config.statsd.scope,
 });
 
-let generationDurationTimer = null;
+client.socket.on('error', (err) => {
+  console.error(err.message);
+});
+
+// let generationDurationTimer = null;
 
 function registerGeneration() {
   if (config.statsd.enabled) {
-    metrics.increment('generations_count');
+    client.increment('generations_count');
     console.log('Incremented generations_count to statsd.');
   }
 }
 
 function setRiotChampionsCount(championsCount) {
   if (config.statsd.enabled) {
-    metrics.count('generation_riot_champions', championsCount);
+    client.gauge('generation_riot_champions', championsCount);
     console.log(`Sent generation_riot_champions to statsd ${championsCount}.`);
   }
 }
 
 function setRiotItemsCount(itemsCount) {
   if (config.statsd.enabled) {
-    metrics.count('generation_riot_items', itemsCount);
+    client.gauge('generation_riot_items', itemsCount);
     console.log(`Sent generation_riot_items to statsd ${itemsCount}.`);
   }
 }
 
 function setGeneratedBuildsCount(buildsCount) {
   if (config.statsd.enabled) {
-    metrics.count('generation_generated_builds', buildsCount);
+    client.gauge('generation_generated_builds', buildsCount);
     console.log(`Sent generation_generated_builds to statsd ${buildsCount}.`);
   }
 }
 
 function startGenerationTimer() {
-  if (config.statsd.enabled) {
-    generationDurationTimer = metrics.createTimer('generation_duration');
-    console.log('Started generation_duration statsd timer.');
-  }
+  // if (config.statsd.enabled) {
+  //   generationDurationTimer = client.createTimer('generation_duration');
+  //   console.log('Started generation_duration statsd timer.');
+  // }
 }
 
 function stopGenerationTimer() {
-  if (config.statsd.enabled && generationDurationTimer) {
-    generationDurationTimer.stop();
-    generationDurationTimer = null;
-    console.log('Stopped generation_duration statsd timer.');
-  }
+  // if (config.statsd.enabled && generationDurationTimer) {
+  //   generationDurationTimer.stop();
+  //   generationDurationTimer = null;
+  //   console.log('Stopped generation_duration statsd timer.');
+  // }
 }
 
 function close() {
-  metrics.close();
+  // client.close();
 }
 
 export {
