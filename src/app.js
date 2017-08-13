9cb3ef19ab5f8a027fff72c655ca3b065bc26da5
Ilshidur <maar-gan@hotmail.fr>
2017-08-13 03:54:02 +0200

    feat: express statsd reporting

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import expressStatsd from 'express-statsd';
 import config from './config';
 import routes from './routes';
 
@@ -7,15 +8,25 @@ const version = require('../package.json').version;
 
 console.log(`Running app version : ${version}.`);
 
+function statsd(scope) {
+  return (req, res, next) => {
+    const method = req.method || 'unknown_method';
+    req.statsdKey = ['http', method.toLowerCase(), scope].join('.');
+    next();
+  };
+}
+
 // ==== Server ====
 
 const app = express();
 
+app.use(expressStatsd());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/sprites', routes.sprites);
-app.use('/tooltips', routes.tooltips);
-app.use('/', routes.index);
+app.use('/sprites', statsd('sprites'), routes.sprites);
+app.use('/tooltips', statsd('tooltips'), routes.tooltips);
+app.use('/', statsd('home'), routes.index);
 
 app.listen(config.port, () => {
   console.log(`[SERVER] Listening on port ${config.port}`);
